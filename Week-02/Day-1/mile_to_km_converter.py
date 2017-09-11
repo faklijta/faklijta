7065e75d094501a91608244e2a9d9728750078aa
faklijta <faklijta@gmail.com>
2017-09-11 15:02:38 +0200

    feat(day1) exercise 15 done

--- Week-02/Day-1/mile_to_km_converter.py
+++ Week-02/Day-1/mile_to_km_converter.py
@@ -1,2 +1,4 @@
 # Write a program that asks for an integer that is a distance in kilometers,
 # then it converts that value to miles and prints it
+x = int(input('add a number of kilometers:'))
+print ('in miles it is:' 'x*0,62')
