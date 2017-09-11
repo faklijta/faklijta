5e6282bc80175835237fdb464edcfd2bbca15a48
faklijta <faklijta@gmail.com>
2017-09-11 13:58:32 +0200

    feat(day1) exercise 10 done

--- Week-02/Day-1/define_basic_info.py
+++ Week-02/Day-1/define_basic_info.py
@@ -3,3 +3,11 @@
 # Your age as an integer
 # Your height in meters as a double
 # Wether you are married or not as a boolean
+name = str('Tania')
+print (name)
+age = int(34)
+print(age)
+height = (1.67**2)
+print (height)
+married = True
+print(married)
