149577a1fc913f0b4b33b3a83a99f3c29d6f8101
faklijta <faklijta@gmail.com>
2017-09-11 14:29:32 +0200

    feat(day) exercise 13 done

--- Week-02/Day-1/seconds_in_a_day.py
+++ Week-02/Day-1/seconds_in_a_day.py
@@ -1,6 +1,12 @@
-current_hours = 14;
-current_minutes = 34;
-current_seconds = 42;
+current_hours = 14
+current_minutes = 34
+current_seconds = 42
 
 # Write a program that prints the remaining seconds (as an integer) from a
 # day if the current time is represented bt the variables
+day = int(60*60*24)
+print (day)
+current_time_in_sec = int(current_hours)*60*60 + int(current_minutes)*60 + int(current_seconds)
+print (current_time_in_sec)
+remaining_time = day - current_time_in_sec
+print (remaining_time)
