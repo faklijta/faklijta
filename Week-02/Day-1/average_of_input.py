300317f5527d013bf6593d8aadb5d44b60eb94e4
faklijta <faklijta@gmail.com>
2017-09-11 15:24:10 +0200

    feat(day1) exersice 17 done

--- Week-02/Day-1/average_of_input.py
+++ Week-02/Day-1/average_of_input.py
@@ -2,3 +2,12 @@
 # then it should print the sum and the average of these numbers like:
 #
 # Sum: 22, Average: 4.4
+a = int(input('add value1'))
+b = int(input('add value2'))
+c = int(input('add value3'))
+d = int(input('add value4'))
+e = int(input('add value5'))
+sum = a+b+c+d+e
+print('Sum:', sum)
+avg = (sum/5)
+print('Average: ', avg)
