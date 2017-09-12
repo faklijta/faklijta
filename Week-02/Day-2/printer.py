2a636473c187b18deb0143f40b4ea827f46d283f
faklijta <faklijta@gmail.com>
2017-09-12 15:35:14 +0200

    feat(day2) review code

--- Week-02/Day-2/printer.py
+++ Week-02/Day-2/printer.py
@@ -1,7 +1,7 @@
 # - Create a function called `printer`
 #   which prints the input parameters
 #   (can have multiple number of arguments)
-
-text= input('please add what to print')
+text = ('apple')
 def printer(*text):
     print (text)
+
