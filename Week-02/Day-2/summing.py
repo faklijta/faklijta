b1847f3970e8e39bd55cc7d9d008ea699df7b5e5
faklijta <faklijta@gmail.com>
2017-09-12 12:33:15 +0200

    feat(day2) exc 4 done

---
+++ Week-02/Day-2/summing.py
@@ -0,0 +1,13 @@
+# - Write a function called `sum` that sum all the numbers
+#   until the given parameter
+numbers = int(input ("please add a number"))
+def sum(numbers):
+        return sum(list(range(1,number+1)))
+    print (sum(numbers))
+
+
+    
+    
+
+ 
+
