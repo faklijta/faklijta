0ff1a9a1d0d2151d326c941e616ef3fe545b6edd
faklijta <faklijta@gmail.com>
2017-09-13 11:13:46 +0200

    feat(day3) exercise 01 in lists complete

---
+++ Week-02/Day-3/soler_system.py
@@ -0,0 +1,7 @@
+# Saturn is missing from the planet_list
+# Insert it into the correct position
+
+planet_list = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune"]
+
+planet_list = planet_list[:4] + ["Saturn"] + planet_list[-2:]
+print(planet_list)
