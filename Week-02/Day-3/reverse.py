39605a132493c3f1710dd945c6019ae14d0964ee
faklijta <faklijta@gmail.com>
2017-09-13 10:56:25 +0200

    feat(day3) exercise 02 complete

---
+++ Week-02/Day-3/reverse.py
@@ -0,0 +1,6 @@
+# Create a function called 'reverse_string' which takes a string as a parameter
+# The function reverses it and returns with the reversed string
+reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI"
+def reversed_string(new_order):
+        return new_order[::-1]
+print(reversed_string(reversed))
