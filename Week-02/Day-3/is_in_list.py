d75b6826dd143223813d3b790ad686c17ef9fe27
faklijta <faklijta@gmail.com>
2017-09-13 16:38:16 +0200

    feat(day3) is_in_list done

---
+++ Week-02/Day-3/is_in_list.py
@@ -0,0 +1,13 @@
+# Check if list contains all of the following elements: 4,8,12,16
+# Create a function that accepts list_of_numbers as an input
+# it should return "True" if it contains all, otherwise "False"
+
+list_of_numbers = [2, 4, 6, 8, 10, 12, 14, 16]
+need_to_contain = [4, 8, 12, 16]
+
+def is_all_in_list(need_to_contain,list_of_numbers):
+    for element in need_to_contain:
+        if not element in list_of_numbers:
+            return False
+    return True
+print(is_all_in_list(need_to_contain,list_of_numbers))
