212dfc48f367ec05fbca00cd3e0ae3d471db8c84
faklijta <faklijta@gmail.com>
2017-09-18 21:49:10 +0200

    feat(day1) diamond complete

--- Week-02/Day-1/draw_diamond.py
+++ Week-02/Day-1/draw_diamond.py
@@ -11,3 +11,15 @@
 #    *
 #
 # The diamond should have as many lines as the number was
+
+nol = int(input('please add a number: '))
+def drawer():
+    for i in range(1,(nol+1)//2+1):
+        print(" "*((nol+1)//2-i)+"*"*(i*2-1))
+    if nol%2 != 0:   
+        for i in range(1,nol//2+1):
+            print(" "*i+"*"*(nol-i*2))
+    else:   
+        for i in range(1,nol//2+1):
+            print(" "*(i-1)+"*"*((nol+1)-i*2))
+drawer()
