a31b5c95fc14f4dd771794d2c4336a4a6dfa4064
faklijta <faklijta@gmail.com>
2017-09-18 21:54:53 +0200

    feat(day1) draw_pyramid done

--- Week-02/Day-1/draw_pyramid.py
+++ Week-02/Day-1/draw_pyramid.py
@@ -8,3 +8,8 @@
 # *******
 #
 # The pyramid should have as many lines as the number was
+nol = int(input('please add a number: '))
+def drawer():
+    for i in range(1,nol+1):
+        print(" "*(nol-i)+"*"*(i*2-1))
+drawer()
