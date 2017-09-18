54d161e3bbe3a6f94983deccd564a79d04f2e2e0
faklijta <faklijta@gmail.com>
2017-09-18 22:46:15 +0200

    feat(day1) guess the number complete

--- Week-02/Day-1/guess_the_number.py
+++ Week-02/Day-1/guess_the_number.py
@@ -5,3 +5,13 @@
 # The stored number is higher
 # The stried number is lower
 # You found the number: 8
+import random
+random_number = random.randint(0,100)
+user_number = int(input("choose a number between 0 and 100: "))
+if user_number != random_number:
+    if user_number > random_number:
+        print('The stored number is lower')
+    if user_number < random_number:
+        print('The stored number is higher')
+if user_number == random_number:
+    print("You found the number: "+ random_number)
