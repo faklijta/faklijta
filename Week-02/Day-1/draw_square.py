815dd10e733324f787bd13ce9a686b6d83f5259d
faklijta <faklijta@gmail.com>
2017-09-18 22:04:13 +0200

    feat(day1) draw square complete

--- Week-02/Day-1/draw_square.py
+++ Week-02/Day-1/draw_square.py
@@ -10,3 +10,11 @@
 # %%%%%
 #
 # The square should have as many lines as the number was
+nol = int(input('please add a number: '))
+def drawer():
+    for i in range(1,nol+1):
+        if i == 1 or i== nol:
+            print("%"*(nol-1))
+        else:
+            print("%"+" "*(nol-3)+"%")
+drawer()
