2e2c52bc8ae47412bd5361c0510317ff10a456cb
faklijta <faklijta@gmail.com>
2017-09-21 11:43:03 +0200

    feat(day4) string2 complete

--- Week-03/Day-4/string2.py
+++ Week-03/Day-4/string2.py
@@ -1,1 +1,10 @@
 # Given a string, compute recursively a new string where all the 'x' chars have been removed.
+
+def x_to_none(string,x):
+    if x not in string:
+        return string
+    if string[0] == x:
+        return "" + x_to_none(string[1:],x)
+    return string[0] + x_to_none(string[1:],x)
+
+print(x_to_none("alex","x"))
