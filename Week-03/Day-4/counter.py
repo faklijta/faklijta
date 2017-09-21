d5d7a5d1cd5b8107a9f60cf1c1572b40ad5bac9c
faklijta <faklijta@gmail.com>
2017-09-21 10:18:35 +0200

    feat(day4) library create

---
+++ Week-03/Day-4/counter.py
@@ -0,0 +1,7 @@
+# Write a recursive function that takes one parameter: n and counts down from n.
+def counter(n):
+    if n == 0:
+        return 0
+    else:
+        return counter(n-1)
+print(counter(5))
