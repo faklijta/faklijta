79956ab237759aec502c427743f6cb3bc91197ea
faklijta <faklijta@gmail.com>
2017-09-21 10:52:50 +0200

    feat(day4) power complete

--- Week-03/Day-4/power.py
+++ Week-03/Day-4/power.py
@@ -1,2 +1,9 @@
 # Given base and n that are both 1 or more, compute recursively (no loops)
 # the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+def power(base,n):
+    if n == 0:
+        return 1
+    else:
+        return base**n
+
+print(power(3,3))
