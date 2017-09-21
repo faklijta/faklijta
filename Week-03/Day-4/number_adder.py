8d1c7a5439beb710a295b0306b70f8c014c7c432
faklijta <faklijta@gmail.com>
2017-09-21 10:23:23 +0200

    feat(day4) number adder complete

--- Week-03/Day-4/number_adder.py
+++ Week-03/Day-4/number_adder.py
@@ -1,1 +1,8 @@
 # Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+def counter(n):
+    if n == 0:
+        return 0
+    else:
+        return n + counter(n-1)
+
+print(counter(4))
