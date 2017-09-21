f046f0849b9c5c48e2885c800f0321296ec5cf25
faklijta <faklijta@gmail.com>
2017-09-21 11:58:20 +0200

    feat(day4) string3 complete

--- Week-03/Day-4/string3.py
+++ Week-03/Day-4/string3.py
@@ -1,2 +1,10 @@
 # Given a string, compute recursively a new string where all the
 # adjacent chars are now separated by a "*".
+
+def add_star(string):
+    if len(str(string)) == 1:
+        return string
+    if string[0]:
+        return string[:1] + "*" + add_star(string[1:])
+
+print(add_star("attila"))
