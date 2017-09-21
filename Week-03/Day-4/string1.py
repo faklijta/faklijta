9c701cfa320a071ef5e348b0b6b2359eeec89f6b
faklijta <faklijta@gmail.com>
2017-09-21 11:37:44 +0200

    feat(day4) string complete

--- Week-03/Day-4/string1.py
+++ Week-03/Day-4/string1.py
@@ -1,2 +1,11 @@
 # Given a string, compute recursively (no loops) a new string where all the
 # lowercase 'x' chars have been changed to 'y' chars.
+
+def x_to_y(string,x,y):
+    if x not in string:
+        return string
+    if string[0] == x:
+        return y + x_to_y(string[1:],x,y)
+    return string[0] + x_to_y(string[1:],x,y)
+
+print(x_to_y("maximilian","x","y"))
