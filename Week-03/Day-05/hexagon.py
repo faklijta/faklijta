1165c97f17b57c6796e4984bc4445d99aa325de4
faklijta <faklijta@gmail.com>
2017-09-22 12:23:10 +0200

    feat(day4) hexagon2 complete

---
+++ Week-03/Day-05/hexagon.py
@@ -0,0 +1,28 @@
+from tkinter import *
+import math
+
+root = Tk()
+canvas = Canvas(root, width=400, height=400)
+
+canvas.pack()
+
+def hexagon(x, y, size):
+    height = math.sqrt(3) / 2 * size
+    canvas.create_polygon(x, y, x + size / 2, y - height, x + size * 1.5, y - height, x + 2 * size, y, 
+                          x + size * 1.5, y + height, x + size / 2, y + height, fill = "", outline = "black")
+def draw_fractal(x, y, size):
+    if size < 1:
+         return
+    else:
+        height = math.sqrt(3) / 2 * size
+        hexagon(x, y, size)
+        draw_fractal(x + size/3, y - height/3*2, size/3)
+        draw_fractal(x + size, y - height/3*2, size/3)
+        draw_fractal(x + size/3, y + height/3*2, size/3)
+        draw_fractal(x + size, y + height/3*2, size/3)
+        draw_fractal(x, y , size/3)
+        draw_fractal(x + size*1.33, y , size/3)
+        # draw_fractal(x + size / 4, y - height / 2, size / 2)
+
+draw_fractal(10,200,150)
+root.mainloop()
