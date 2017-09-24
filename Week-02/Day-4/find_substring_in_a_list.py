7704bae67221c55632d3a5f9778e3a82a7e980fb
faklijta <faklijta@gmail.com>
2017-09-24 17:11:24 +0200

    feat(day4) subint finalize

---
+++ Week-02/Day-4/find_substring_in_a_list.py
@@ -0,0 +1,16 @@
+# Find the substring in the list
+
+# Create a function that takes a string and a list of string as a parameter
+# Returns the index of the string in the list where the first string is part of
+# Returns -1 if the string is not part any of the strings in the list
+string_list = ["this", "is", "what", "I'm", "searching", "in"]
+text = "ching" 
+
+def finder(text, string_list):
+    text_index = []
+    for i in range(len(string_list)):
+        if text in string_list[i]:
+            text_index.append(i)
+    return text_index
+
+print(finder(text, string_list))
--- Week-02/Day-4/subint.py
+++ Week-02/Day-4/subint.py
@@ -1,13 +1,17 @@
 # Create a function that takes a number and a list of numbers as a parameter
-# Returns the indeces of the numbers in the list where the first number is part of
+# Returns the indexes of the numbers in the list where the first number is part of
 # Returns an empty list if the number is not part any of the numbers in the list
 #input = [1, 11, 34, 52, 61], 1
 
 input_list = [1, 11, 34, 52, 61]
 input_number = 1
 
-def integer_finder(input_number,input_list):
-    
+def find_integer(input_list, input_number):
+    what_to_find = str(input_number)
+    indexes = []
     for i in range(len(input_list)):
-
-integer_finder(input_list,input_number)
+        list_index = str(input_list[i])
+        if what_to_find in list_index:
+            indexes.append(i)
+    return indexes
+print(find_integer(input_list,input_number))
