0825d8aaf62d470f78bb54c14085bc52d2ce0de2
faklijta <faklijta@gmail.com>
2017-09-25 15:21:52 +0200

    fix(day1) teachers and students complete

---
+++ Week-04/Day-1/teachers_and_students.py
@@ -0,0 +1,12 @@
+class Teacher(object):
+
+    def teach(student):
+        student.learn()
+    def answer(self)
+
+
+class Student(object):
+    
+    def learn(self):
+    def question(teacher):
+        teacher.answer()
