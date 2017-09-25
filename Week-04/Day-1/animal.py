2b3cfc49b6f719b6af6ee9ff40af6274f576fd74
faklijta <faklijta@gmail.com>
2017-09-25 13:57:16 +0200

    feat(day1) animal complete

--- Week-04/Day-1/animal.py
+++ Week-04/Day-1/animal.py
@@ -1,15 +1,18 @@
 class Animals(object):
 
-    def __init__(self, hunger, thirst):
-        self.thirst = thirst
-        self.hunger = hunger
+    def __init__(self):
+        self.thirst = 50
+        self.hunger = 50
 
     def eat(self):
         self.hunger -= 1
+        print(self.hunger)
     
     def drink(self):
         self.thirst -= 1
+        print(self.thirst)
     
     def play(self):
         self.hunger += 1
         self.hunger += 1
+        print(self.hunger, self.thirst)
