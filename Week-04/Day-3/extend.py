305103f0bd71be874a1e72b349ea5d2b7d128a32
faklijta <faklijta@gmail.com>
2017-09-27 17:06:49 +0200

    feat(test and extention) till vovel all functions and tests complete

---
+++ Week-04/Day-3/extend.py
@@ -0,0 +1,33 @@
+import statistics
+
+# Adds a and b, returns as result
+def add(a, b):
+    return a + b
+
+# Returns the highest value from the three given params
+def max_of_three(a, b, c):
+    if a > b:
+        if a > c:
+            return a
+    if c > a:
+        if c > b:
+            return c
+    if b > a:
+        if b > c:
+            return b
+
+# Returns the median value of a list given as param
+def median(pool):
+    return statistics.median(pool)
+
+# Returns true if the param is a vovel
+def is_vovel(char):
+    return char.lower() in 'aeiouéáőűöüóí'
+
+# Create a method that translates hungarian into the teve language
+def translate(hungarian):
+    teve = hungarian
+    for char in teve:
+        if is_vovel(char):
+            teve = (char+'v'+char).join(teve.split(char))
+    return teve
