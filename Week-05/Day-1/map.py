b0ec7dbac5f06443cdd3a3001b8fae3e7f6884d7
faklijta <faklijta@gmail.com>
2017-10-02 15:00:18 +0200

    feat(RPG game) draw map complete

--- Week-05/Day-1/map.py
+++ Week-05/Day-1/map.py
@@ -1,27 +1,39 @@
 from tkinter import *
-from PIL import Image, ImageTk
 
 root = Tk()
 canvas = Canvas(root, width='720', height= '720')
-# tilemap = [[0,0,0,0,0,0,0,0,0,0]]
+tilemap = [[0,0,0,1,0,1,0,0,0,0],
+           [0,0,0,1,0,1,0,1,1,0],
+           [0,1,1,1,0,1,0,1,1,0],
+           [0,0,0,0,0,1,0,0,0,0],
+           [1,1,1,1,0,1,1,1,1,0],
+           [0,1,0,1,0,0,0,0,1,0],
+           [0,1,0,1,0,0,0,0,1,0],
+           [0,0,0,0,0,0,0,0,1,0],
+           [0,1,1,1,0,0,0,0,1,0],
+           [0,0,0,1,0,0,1,0,0,0]]
 
 
 # class Map(object):
 #     def __init__(self, x, y, size):
 #         self.rect = canvas.create_rectangle(x, y, x+size, y+size)
 
-#     def is_wall():
-#         pass
-
+#def is_wall():
+#pass
 
 # image = canvas.create_image(0, 0, anchor=NW, image=filename)
 # def get_tile(x, y):
-filename = PhotoImage(file="floor.png")
+floor = PhotoImage(file="floor.png")
+wall = PhotoImage(file="wall.png")
+
 x = 0
 y = 0
-for row in range(10):
-    for column in range(10):
-        canvas.create_image(x, y, anchor=NW, image=filename)
+for row in range(len(tilemap)):
+    for column in range(len(tilemap[0])):
+        if tilemap[row][column] == 1:
+            canvas.create_image(x, y, anchor=NW, image=wall)
+        else:
+            canvas.create_image(x, y, anchor=NW, image=floor)
         x += 72
     x = 0
     y += 72
