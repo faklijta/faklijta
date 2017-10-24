00bea949e64982c3be77fa700c62575c78a8962e
faklijta <faklijta@gmail.com>
2017-10-24 11:45:40 +0200

    feat(define-basic-info) complete

---
+++ Week-07/Day-1/define-basic-info.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+let name = 'Tania';
+let age = 33;
+let height = 1.65;
+let maritalStatus = true;
+
+console.log(name + ' ' + age + ' ' + height + ' ' + 'Married: ' + maritalStatus)
