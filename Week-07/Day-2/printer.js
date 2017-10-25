36ddbfeda27fed66e52b721b2c719eb00f6b011e
faklijta <faklijta@gmail.com>
2017-10-25 11:01:13 +0200

    style(factorio) whitespaces correct

--- Week-07/Day-2/printer.js
+++ Week-07/Day-2/printer.js
@@ -3,9 +3,9 @@
 //   which logs to the console the input parameters
 //   (can have multiple number of arguments)
 
-function printer(input){
+function printer(input) {
     var parameter_list = ""
-    for(var i = 0; i <= arguments.length - 1; i++){
+    for(var i = 0; i <= arguments.length - 1; i++) {
         parameter_list += arguments[i] + '\n';
     }
     console.log(parameter_list)
