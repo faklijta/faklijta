6b22f2416231621824070873e085e47487965c0e
faklijta <faklijta@gmail.com>
2017-10-25 09:15:09 +0200

    feat(doubling) complete

---
+++ Week-07/Day-2/doubling.js
@@ -0,0 +1,11 @@
+'use strict';
+// - Create a variable named `ak` and assign the value `123` to it
+// - Create a function called `doubling` that doubles it's input parameter
+// - Log the result of `doubling(ak)` to the console
+
+let ak = 123;
+function doubling(input) {
+    var newInput = input * 2;
+    return newInput;
+}
+console.log(doubling(ak))
