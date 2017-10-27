ec072126e2b8c2b516fdd01a6eebab9f3b7de089
faklijta <faklijta@gmail.com>
2017-10-27 12:15:42 +0200

    feat(galery) js database complete

---
+++ Week-07/Day-4/script.js
@@ -0,0 +1,7 @@
+let galery = [{'title': 'Boys', 'description': 'Cuties', 'url': 'images/img_01.jpg'}, 
+{'title': 'Siblings', 'description': 'Under the tree', 'url': 'images/img_02.jpg'},
+{'title': 'Alex', 'description': 'Big boy', 'url': 'images/img_03.jpg'},
+{'title': 'Us', 'description': 'Our family', 'url': 'images/img_04.jpg'},
+{'title': 'Maxim', 'description': 'Thinking', 'url': 'images/img_05.jpg'},
+{'title': 'Maxim2', 'description': 'Portrait', 'url': 'images/img_06.jpg'}]
+
