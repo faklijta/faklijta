185740167618ab823997bbf921c0936bd4fcc17d
faklijta <faklijta@gmail.com>
2017-10-31 14:27:40 +0100

    feat(call) complete

---
+++ week-08/Day-2/call.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function factorialTillLimitWithCallback(limit, callback) {
+  var factorial = 1;
+  for (var i = 1; i <= limit; ++i) {
+    callback(factorial);
+    factorial *= i;
+  }
+}
+function printer(factorial) {
+    console.log('the number you are looking for is ' + factorial)
+}
+
+factorialTillLimitWithCallback(20,printer)
+// Create a function and pass it as a parameter to the factorialTillLimitWithCallback
+// function, and print all the factorial numbers till 20
