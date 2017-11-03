a494d82edcd954b99001d80bba6b4511df6e97bd
faklijta <faklijta@gmail.com>
2017-11-03 09:27:37 +0100

    feat(giphy) foxes are moving on click

---
+++ week-08/day-04/jquery.js
@@ -0,0 +1,8 @@
+var myDiv = $('<div id="myDiv">This is a div.</div>');
+ 
+//myDiv now represents a jQuery object referencing a DOM node.
+ 
+var someText = $('<p/>');
+//someText is a jQuery object referencing an HTMLParagraphElement
+ 
+var input = $('<input />');
