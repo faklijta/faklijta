8cdf4578fc8e1ec2de04599635ef3888fbe87ac4
faklijta <faklijta@gmail.com>
2017-11-03 15:13:12 +0100

    refact(giphy) unnesary variables and functions deleted

--- week-08/day-04/jquery.js
+++
@@ -1,8 +0,0 @@
-var myDiv = $('<div id="myDiv">This is a div.</div>');
- 
-//myDiv now represents a jQuery object referencing a DOM node.
- 
-var someText = $('<p/>');
-//someText is a jQuery object referencing an HTMLParagraphElement
- 
-var input = $('<input />');
