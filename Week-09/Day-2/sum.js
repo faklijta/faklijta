c9d9b11c6f1731cd6314f81ed18e52052482c2e2
faklijta <faklijta@gmail.com>
2017-11-07 17:34:42 +0100

    feat(test_add) complete

--- Week-09/Day-2/sum.js
+++ Week-09/Day-2/sum.js
@@ -1,10 +1,21 @@
 listOfNumbers = [1, 4];
 
 let summing = function(myArray) {
-    total = 0;
+    let total = 0;
+    if (myArray !== null) {
     for (i=0; i < myArray.length; i++ ){
         total += myArray[i];
     } return total;
+} else {
+    return new Error("not valid input");
+}
+};
+
+try {
+    summing(null);
+}
+catch(ex) {
+    t.ok(false)
 }
 
 module.exports = summing;
--- Week-09/Day-2/test_sum.js
+++ Week-09/Day-2/test_sum.js
@@ -1,12 +1,52 @@
 'use strict';
 
 var test = require('tape');
-var sumList = require('./sum');
+var summing = require('./sum');
 
 test('sum two elements', function (t) {
-  var actual = sumList([1, 2]);
-  var expected = 3;
+  var actual = summing([1, 4]);
+  var expected = 5;
 
   t.equal(actual, expected);
   t.end();
 });
+
+test('empty list', function (t) {
+    var actual = summing([]);
+    var expected = 0;
+  
+    t.equal(actual, expected);
+    t.end();
+  });
+
+  test('sum one elements', function (t) {
+    var actual = summing([1]);
+    var expected = 1;
+  
+    t.equal(actual, expected);
+    t.end();
+  });
+
+  test('sum multiple elements', function (t) {
+    var actual = summing([1, 2, 3, 4]);
+    var expected = 10;
+  
+    t.equal(actual, expected);
+    t.end();
+  });
+
+//   test('sum null', function (t) {
+//     var actual = summing(null);
+//     var expected = 'not valid input';
+  
+//     t.equal(actual, expected);
+//     t.end();
+//   });
+
+//   test('sum string', function (t) {
+//     var actual = summing(["hello"]);
+//     var expected = 'not valid input';
+  
+//     t.equal(actual, expected);
+//     t.end();
+//   });
