0fff2929b682fbb4a553a1a2fa20ad580b2b5cc2
faklijta <faklijta@gmail.com>
2017-11-08 16:06:31 +0100

    feat(dountil)complete

--- Week-09/Day-3/index.js
+++ Week-09/Day-3/index.js
@@ -1,7 +1,10 @@
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
+let urlencodedParser = bodyParser.urlencoded({extended :false});
 
 app.use('/assets', express.static('./assets'));
+app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
     response.sendFile(__dirname + '/index.html');
@@ -29,6 +32,27 @@ app.get('/greeter', function(request, response){
 
 app.get('/appenda/:input', function(request, response){
     response.json({'appended' : request.params.input + 'a'});
+});
+
+app.post('/dountil/:input', urlencodedParser, function(request, response){
+    if (request.params.input === 'sum'){
+        let sum = 0;
+        let num = request.body.until;
+        while (num > 0){
+            sum += num;
+            num--;
+        } 
+        response.json({'result' : sum});
+    }
+    if (request.params.input === 'factor'){
+        let num = request.body.until;
+        let fact = 1;
+        while (num > 0){
+            fact *= num;
+            num--;
+        }
+        response.json({'result' : fact});
+    }
 });
 
 app.listen(8080);
