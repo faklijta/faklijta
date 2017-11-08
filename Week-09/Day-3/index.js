325eb35184eb7a4ff5c999b3b66ad5b2b7b68e2f
faklijta <faklijta@gmail.com>
2017-11-08 12:44:10 +0100

    feat(doubling)returning the double of input complete

--- Week-09/Day-3/index.js
+++ Week-09/Day-3/index.js
@@ -1,11 +1,15 @@
 let express = require('express');
 let app = express();
 
-app.use('./assets', express.static('./assets'));
+app.use('/assets', express.static('./assets'));
 
 app.get('/', function(request, response) {
     response.sendFile(__dirname + '/index.html');
 });
 
+app.get('/doubling', function(request, response){
+    response.json({'received' : request.query.input, 'result' : request.query.input*2});
+});
 
-app.listen(3000);
+
+app.listen(8080);
