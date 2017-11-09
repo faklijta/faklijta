772da8edd2c8e1d9ba8290504e8be45944c6f44f
faklijta <faklijta@gmail.com>
2017-11-09 16:05:27 +0100

    feat(bookstore) html complete

--- Week-09/Day-4/bookstore_server.js
+++ Week-09/Day-4/bookstore_server.js
@@ -1,32 +1,42 @@
-let mysql = require("mysql");
-let express = require("express");
-let app = express();
+'use strict';
 
-let conn = mysql.createConnection({
+var express = require('express');
+var mysql = require("mysql");
+var app = express();
+var conn = mysql.createConnection({
   host: "localhost",
-  user: "root",
+  user: "'root'",
   password: "root",
-  database: 'bookstore'
+  database: "bookstore"
 });
+
+app.use('/assets', express.static('./assets'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/bookstore.html');    
+});
+
+app.get('/title', function(req, res) {
+    conn.query('SELECT book_name FROM book_mast;', function(error, rows){
+        if(error) {
+            console.log(error.toString());
+        }
+        let htmlString = '<tr>';
+        rows.forEach(function(row) {
+            htmlString = htmlString + '<tr><td>' + row.book_name + '</td></tr>';
+        });
+        htmlString = htmlString + '</tr>';
+        res.send(htmlString)
+        });
+});
+
 
 conn.connect(function(err){
   if(err){
     console.log("Error connecting to Db");
+    return;
   }
-  else {
-    console.log("Connection established");
-  }
-});
-
-
-app.get('/', function(req, res) {
-  conn.query('SELECT book_name FROM book_mast;',function(err,rows){
-    if(err) {
-      console.log(err.toString());
-      return;
-    }
-    res.send(rows);
-  });
+  console.log("Connection established");
 });
 
 app.listen(3000);
