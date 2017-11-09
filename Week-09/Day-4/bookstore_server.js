f598ed8d5bd2a285f67d855ef913b4404aa567c4
faklijta <faklijta@gmail.com>
2017-11-09 16:56:54 +0100

    feat(bookstore) server returns all the data needed in a table

--- Week-09/Day-4/bookstore_server.js
+++ Week-09/Day-4/bookstore_server.js
@@ -16,14 +16,24 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/bookstore.html');    
 });
 
-app.get('/title', function(req, res) {
-    conn.query('SELECT book_name FROM book_mast;', function(error, rows){
+app.get('/allData', function(req, res) {
+    conn.query('SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast\
+               JOIN author ON author.aut_id=book_mast.aut_id\
+                JOIN category ON book_mast.cate_id=category.cate_id \
+                JOIN newpublisher ON book_mast.pub_id=newpublisher.pub_id;', 
+                function(error, rows){
         if(error) {
             console.log(error.toString());
         }
+        // let book_title = [rows[].book_name]
         let htmlString = '<tr>';
         rows.forEach(function(row) {
-            htmlString = htmlString + '<tr><td>' + row.book_name + '</td></tr>';
+            htmlString = htmlString + `<tr><td>` + row.book_name + `</td>
+                                      <td>` + row.aut_name + `</td>
+                                      <td>` + row.cate_descrip + `</td>
+                                      <td>` + row.pub_name + `</td>
+                                      <td>` + row.book_price + `</td>
+                                      </tr>`;
         });
         htmlString = htmlString + '</tr>';
         res.send(htmlString)
